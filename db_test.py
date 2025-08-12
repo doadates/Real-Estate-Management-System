dfa6520196e37abbbe8a4522c24f40ebb03df57b
Doga Deniz Ates <105700748+doadates@users.noreply.github.com>
2025-08-12 16:38:18 +0200

    Delete db_test.py

--- db_test.py
+++
@@ -1,9 +0,0 @@
-from database import get_connection
-
-conn = get_connection()
-if conn:
-    print("Connected to database successfully.")
-    conn.close()
-else:
-    print("Connection failed.")
-    
